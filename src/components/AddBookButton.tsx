b86e9506180761c80535685eccf874bdb9f63756
Nishant Sharma <nishantsharma2633@gmail.com>
2024-04-24 13:19:06 -0400

    prisma setup

---
+++ src/components/AddBookButton.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+export default function AddBookButton() {
+  return (
+    <div className="flex justify-center">
+      <button
+        className="py-2 px-4 rounded-lg hover:bg-white border-gray-400 border-2 hover:text-black"
+        onClick={() => {}}
+      >
+        Add Book
+      </button>
+    </div>
+  );
+}
